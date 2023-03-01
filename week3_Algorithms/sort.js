7f6a05ec00701aab5347e1b395ab963a69a7ddaf
Yan Shtein <85824489+YanShtein@users.noreply.github.com>
2023-03-01 15:45:49 +0200

    small changes

--- week3_Algorithms/sort.js
+++ week3_Algorithms/sort.js
@@ -1,18 +1,18 @@
 let print = console.log;
 
+let bufferVar = "#";
+
 function main() {
   let height = 4; // asking the user
 
-  draw(height);
+  draw(height, bufferVar);
 }
 
-function draw(n) {
+function draw(n, bufferVar) {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < i+1; j++) {
-      print("#");
-      
+      print(bufferVar);
     }
-
   }
 }
 
